1516e394c562685f7c9667fccf826bd21364beca
Rodrigo Wehbe <falcaorw@gmail.com>
2018-10-20 15:28:16 -0300

    finalização do algoritmo e treinamento da rede

--- dataTest.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-#df = pd.read_csv("data/dota2Test.csv", ",")
-df = pd.read_csv("data/heroes.csv", ",")
-
-print(df.size)
--- neural.py
+++ neural.py
@@ -2,43 +2,89 @@ import numpy as np
 from keras.utils import to_categorical
 from keras import models
 from keras import layers
-from keras.datasets import imdb
+from keras.models import load_model
 import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-#dados de treinamento
-trainDf = pd.read_csv("data/dota2Train.csv", ",")
-trainWinners = trainDf.winnerTeam
-trainHeroPicks = np.array(trainDf.values)
-trainHeroPicks = trainHeroPicks[:,4:]
+def winnerValuesTreatment(winners):
+    treated = list()
+    for item in winners:
+        if item == -1:
+            treated.append([0,1]) 
+            
+        else:
+            treated.append([1,0])
+    return treated
 
-#dados de teste
-testDf = pd.read_csv("data/dota2Test.csv", ",")
-testWinners = testDf.winnerTeam
-testHeroPicks = np.array(testDf.values)
-testHeroPicks = testHeroPicks[:,4:]
+def neuralNetwork():
+    #dados de treinamento
+    trainDf = pd.read_csv("data/dota2Train.csv", ",")
+    trainWinners = np.array(winnerValuesTreatment(trainDf.winnerTeam))
+    trainHeroPicks = np.array(trainDf.values)
+    trainHeroPicks = trainHeroPicks[:,4:]
 
- 
-model = models.Sequential()
-# Input - Layer
-model.add(layers.Dense(50, activation = "relu", input_shape=(113, )))
-# Hidden - Layers
-model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
-model.add(layers.Dense(50, activation = "relu"))
-model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
-model.add(layers.Dense(50, activation = "relu"))
-# Output- Layer
-model.add(layers.Dense(1, activation = "sigmoid"))
+    #dados de teste
+    testDf = pd.read_csv("data/dota2Test.csv", ",")
+    testWinners = np.array(winnerValuesTreatment(testDf.winnerTeam))
+    testHeroPicks = np.array(testDf.values)
+    testHeroPicks = testHeroPicks[:,4:]
 
-model.summary()
+    
+    model = models.Sequential()
+    # Input - Layer
+    model.add(layers.Dense(1024, activation = "relu", input_shape=(113, )))
+    # Hidden - Layers
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(512, activation = "softmax"))
+    model.add(layers.Dense(512, activation = "relu"))
+    model.add(layers.Dense(512, activation = "relu"))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(256, activation = "relu"))
+    # Output- Layer
+    model.add(layers.Dense(2, activation = "softmax"))
+    
+    
+    #model.summary()
 
-# compiling the model
-model.compile(
- optimizer = "adam",
- loss = "binary_crossentropy",
- metrics = ["accuracy"]
-)
+    #compiling the model
+    #model.compile(
+    #optimizer = "adam",
+    #loss = "categorical_crossentropy",
+    #metrics = ["accuracy"]
+    #)
 
-fitResult = model.fit(trainHeroPicks, trainWinners, epochs=10, batch_size=32, verbose=1)
+    model = load_model('models/categoricalModel.h5')
 
-predicted = model.predict(testHeroPicks)
-print(predicted)
+    fitResult = model.fit(trainHeroPicks, trainWinners, epochs=1, batch_size=1024, verbose=1)
+
+    predicted = model.predict(testHeroPicks)
+    print(predicted)
+    
+    model.save('models/categoricalModel.h5')
+
+    predict = list()
+    test_labels = list()
+    for value in predicted:
+        predict.append(0 if value[0] < value[1] else 1)
+    for value in testWinners:
+        test_labels.append(0 if value[0] < value[1] else 1)
+
+
+    mat = confusion_matrix(test_labels, predict)
+    plt.figure(figsize=(8, 8))
+    sns.set()
+    sns.heatmap(mat.T, square=True,
+                xticklabels=np.unique(test_labels),
+                yticklabels=np.unique(test_labels))
+    # annot=True,
+    plt.xlabel('true label')
+    plt.ylabel('predicted label')
+    # Save confusion matrix to outputs in Workbench
+    # plt.savefig(os.path.join('.', 'outputs', 'confusion_matrix.png'))
+    print(mat)
+    plt.show()
+
+    return predicted
+
